59814681a9c07752c67ebf03c33befc8f9466b0c
Sayantan Chatterjee <thisissayantan.mail@gmail.com>
2017-10-09 01:16:15 +0530

    Edit Romanian City Names

--- IIIT-A Lab (IAIN532C)/Lab 2/romanian_paths.py
+++ IIIT-A Lab (IAIN532C)/Lab 2/romanian_paths.py
@@ -4,16 +4,16 @@ GRAPH = {\
             'Arad': {'Sibiu': 140, 'Zerind': 75, 'Timisoara': 118},\
             'Zerind': {'Arad': 75, 'Oradea': 71},\
             'Oradea': {'Zerind': 71, 'Sibiu': 151},\
-            'Sibiu': {'Arad': 140, 'Oradea': 151, 'Faragas': 99, 'Rimnicu': 80},\
+            'Sibiu': {'Arad': 140, 'Oradea': 151, 'Fagaras': 99, 'Rimnicu': 80},\
             'Timisoara': {'Arad': 118, 'Lugoj': 111},\
             'Lugoj': {'Timisoara': 111, 'Mehadia': 70},\
-            'Mehadia': {'Lugoj': 70, 'Dobreta': 75},\
-            'Dobreta': {'Mehadia': 75, 'Craiova': 120},\
-            'Craiova': {'Dobreta': 120, 'Rimnicu': 146, 'Pitesti': 138},\
+            'Mehadia': {'Lugoj': 70, 'Drobeta': 75},\
+            'Drobeta': {'Mehadia': 75, 'Craiova': 120},\
+            'Craiova': {'Drobeta': 120, 'Rimnicu': 146, 'Pitesti': 138},\
             'Rimnicu': {'Sibiu': 80, 'Craiova': 146, 'Pitesti': 97},\
-            'Faragas': {'Sibiu': 99, 'Bucharest': 211},\
+            'Fagaras': {'Sibiu': 99, 'Bucharest': 211},\
             'Pitesti': {'Rimnicu': 97, 'Craiova': 138, 'Bucharest': 101},\
-            'Bucharest': {'Faragas': 211, 'Pitesti': 101, 'Giurgiu': 90, 'Urziceni': 85},\
+            'Bucharest': {'Fagaras': 211, 'Pitesti': 101, 'Giurgiu': 90, 'Urziceni': 85},\
             'Giurgiu': {'Bucharest': 90},\
             'Urziceni': {'Bucharest': 85, 'Vaslui': 142, 'Hirsova': 98},\
             'Hirsova': {'Urziceni': 98, 'Eforie': 86},\
